0d451fd19eea5fa9aa0848266fe28345a2fada6d
Janynne <janynne.matter@hotmail.com>
2021-06-01 19:27:00 -0400

    Effects to the page (like shadows)

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const descriptionOfProject = document.querySelector('.description-project')
 const buttonColor = document.querySelector('.color-project')
 const backgroundColor = document.querySelector('.text-content-container')
 
-const buttonMobile = document.querySelector('.button-mobile')
-
 const buttonSave = document.querySelector('.save-project')
 
 //text editor
@@ -31,23 +29,6 @@ function selectColor() {
     backgroundColor.style.backgroundColor=color
 }
 buttonColor.addEventListener('input', selectColor);
-
-//hamburguer button mobile
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const menuNav = document.querySelector('.menu-nav')
-    menuNav.classList.toggle('active');
-    const active = menuNav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expended', active)
-    if (active) {
-        event.currentTarget.setAttribute('aria-label', 'Fermer Menu')
-    } else {
-        event.currentTarget.setAttribute('aria-label', 'Ouvrir Menu')
-    }
-}
-
-buttonMobile.addEventListener('click', toggleMenu)
-buttonMobile.addEventListener('touchstart', toggleMenu)
 
 //button save
 buttonSave.addEventListener('click', (event) => {
---
+++ sharedcontent.js
@@ -0,0 +1,34 @@
+const buttonMobile = document.querySelector('.button-mobile')
+
+const navMobile = document.querySelector('.link-nav-ul')
+const userMobile = document.querySelector('.user-photo-head-of-page')
+
+//hamburguer button mobile
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const menuNav = document.querySelector('.menu-nav')
+    menuNav.classList.toggle('active');
+    const active = menuNav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expended', active)
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fermer Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Ouvrir Menu')
+    }
+}
+
+buttonMobile.addEventListener('click', toggleMenu)
+buttonMobile.addEventListener('touchstart', toggleMenu)
+
+
+//user profile button mobile
+window.addEventListener('resize', () => {
+    let size = window.innerWidth
+    console.log(size)
+    if(size < 400) {
+        navMobile.append(userMobile)
+    }else {
+        document.querySelector('.head-of-page').append(userMobile)
+    }
+}
+)
